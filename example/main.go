541c039d63ff0bf0eb631b5f56f3edf9c9a32f3e
Tran Hau <ngtranhau@gmail.com>
2021-04-17 20:45:49 +0700

    chore(example): add example for Replace

--- example/main.go
+++ example/main.go
@@ -8,12 +8,22 @@ import (
 
 func main() {
 	// Copy file
-	if err := copy.Copy("copy.go", "copy.go_cloned"); err != nil {
+	if err := copy.Copy("copy.go", "copy1.go"); err != nil {
 		fmt.Println(err)
 	}
 
 	// Copy dir
-	if err := copy.Copy("example", "example_cloned"); err != nil {
+	if err := copy.Copy("example", "example1"); err != nil {
+		fmt.Println(err)
+	}
+
+	// Replace file
+	if err := copy.Replace("copy1.go", "copy2.go"); err != nil {
+		fmt.Println(err)
+	}
+
+	// Replace dir
+	if err := copy.Replace("example1", "example2"); err != nil {
 		fmt.Println(err)
 	}
 }
