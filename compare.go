3355e104ea22239335dda2ff032e93c48de91e10
Nguyen Tran Hau <hauvipapro@gmail.com>
2021-04-23 10:07:46 +0700

    feat: add color when compare, straight to stdout (#4)
    Co-authored-by: Tran Hau <ngtranhau@gmail.com>

--- compare.go
+++ compare.go
@@ -1,58 +1,55 @@
 package copy
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"os"
 
 	"github.com/pkg/diff"
+	"github.com/pkg/diff/write"
 )
 
 // Compare src with dst
 // return error if src is dir not file
-func Compare(src, dst string) (string, error) {
+func Compare(src, dst string) error {
 	src, err := trimHomeSymbol(src)
 	if err != nil {
-		return "", fmt.Errorf("failed to trim ~ for src %s", src)
+		return fmt.Errorf("failed to trim ~ for src %s", src)
 	}
 
 	dst, err = trimHomeSymbol(dst)
 	if err != nil {
-		return "", fmt.Errorf("failed to trim ~ for dst %s", dst)
+		return fmt.Errorf("failed to trim ~ for dst %s", dst)
 	}
 
 	return compareRaw(src, dst)
 }
 
-func compareRaw(src, dst string) (string, error) {
+func compareRaw(src, dst string) error {
 	fileInfo, err := os.Stat(src)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			return "", nil
+			return nil
 		}
 
-		return "", fmt.Errorf("failed to stat src %s: %w", src, err)
+		return fmt.Errorf("failed to stat src %s: %w", src, err)
 	}
 
 	if fileInfo.IsDir() {
-		return "", fmt.Errorf("currently not support compare dir")
+		return fmt.Errorf("currently not support compare dir")
 	} else {
-		result, err := compareFile(src, dst)
-		if err != nil {
-			return "", fmt.Errorf("failed to compare file src %s dst %s: %w", src, dst, err)
+		if err := compareFile(src, dst); err != nil {
+			return fmt.Errorf("failed to compare file src %s dst %s: %w", src, dst, err)
 		}
 
-		return result, nil
+		return nil
 	}
 }
 
-func compareFile(src, dst string) (string, error) {
-	buf := new(bytes.Buffer)
-
-	if err := diff.Text(src, dst, nil, nil, buf); err != nil {
-		return "", fmt.Errorf("failed to diff text src %s dst %s:%w", src, dst, err)
+func compareFile(src, dst string) error {
+	if err := diff.Text(src, dst, nil, nil, os.Stdout, write.TerminalColor()); err != nil {
+		return fmt.Errorf("failed to diff text src %s dst %s:%w", src, dst, err)
 	}
 
-	return buf.String(), nil
+	return nil
 }
